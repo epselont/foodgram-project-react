9b6eecae3b386390bc0daddfe98953b00d9eebee
epselont <newrol35@gmail.com>
2022-09-21 13:54:34 +0300

    Added a models of Ingredients in models and to the admin

--- backend/foodgram/recipes/admin.py
+++ backend/foodgram/recipes/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Ingredients
+
+
+@admin.register(Ingredients)
+class AdminIngredients(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'measurement_unit',)
+    search_fields = ('name',)
+    list_filter = ('measurement_unit',)
+    empty_value_display = '-пусто-'
