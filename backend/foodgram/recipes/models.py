a5500ae9bba0f002bd1c9ff3d18e1209555d3523
epselont <newrol35@gmail.com>
2022-09-21 13:54:34 +0300

    Added a model of Ingredients in models and to the admin

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,1 +1,20 @@
 from django.db import models
+
+
+class Ingredients(models.Model):
+    name = models.CharField(
+        'Название ингредиента',
+        max_length=200
+    )
+    measurement_unit = models.CharField(
+        'Единица измерения',
+        max_length=200
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Ингредиент'
+        verbose_name_plural = 'Ингредиенты'
+        ordering = ['name']
