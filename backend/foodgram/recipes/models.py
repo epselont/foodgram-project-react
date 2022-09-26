f42d80259cc9a1b433780388da38ddea177dfe8e
epselont <newrol35@gmail.com>
2022-09-26 15:58:07 +0300

    Added a model of tag in models

--- backend/foodgram/recipes/models.py
+++ backend/foodgram/recipes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Ingredients(models.Model):
+class Ingredient(models.Model):
     name = models.CharField(
         'Название ингредиента',
         max_length=200
@@ -17,4 +17,30 @@ class Ingredients(models.Model):
     class Meta:
         verbose_name = 'Ингредиент'
         verbose_name_plural = 'Ингредиенты'
+        ordering = ['name']
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        'Название тэга',
+        max_length=200,
+        unique=True
+    )
+    color = models.CharField(
+        'Цвет в формате HEX',
+        max_length=7,
+        unique=True
+    )
+    slug = models.SlugField(
+        'Уникальный slug тэга',
+        max_length=200,
+        unique=True
+    )
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
         ordering = ['name']
