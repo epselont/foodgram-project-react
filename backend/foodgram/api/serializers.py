9de35a1db9b07eca05352d067b38bd376295c5e3
epselont <newrol35@gmail.com>
2022-10-17 14:20:02 +0300

    changed the User serializer in views

--- backend/foodgram/api/serializers.py
+++ backend/foodgram/api/serializers.py
@@ -1,4 +1,5 @@
 from django.contrib.auth import get_user_model
+from djoser.serializers import UserSerializer
 from recipes.models import Ingredient, IngredientsRecipe, Recipe, Tag
 from rest_framework import serializers
 
